5df87719bac950ff4d2349f42a969378b1c93c81
seliv55 <selivanov@ub.edu>
2021-08-31 14:51:57 +0200

    new branch

--- mex/nums.hh
+++
@@ -1,6 +0,0 @@
-extern const int  nqh, npsi, nnad, npyr, nsuc, nfum, noaa, nakgm, nglu, ngluo, naspm, nca, nc1ros, nc2ros, nc3ros, nk_i, npsio, nNa_i, nAdp, nAtp, neo, nei, nlac, numx;
-extern int kmax,kount,ifn,istor,nrhs;
-extern const int  qp_FS, rqp_FS, FS_c1, rFS_c1, qp_bl, rqp_bl, bl_bh, rbl_bh, bh_qn1, rbh_qn1, bh_qn2, rbh_qn2, qHbnd, rqHbnd, qnbnd, rqnbnd, qpdis, rqpdis, qhnds,rqhnds, vc1c, bypas, vros, vfadf, vfadr, vbq1, vrbq1, vbq2, vrbq2, vqdis, vrqdis, vqbind, vrqbind, vfred, vrfred, vn56, vrn56, vn2qn1, vrn2qn1, vn2qn2, vrn2qn2, vndis, vrndis, vpbind, vrpbind, ptp, vlk, vatsyn, vmalic, vmdh, vK, akgsuc, vglu_akg, vatpase, vglu_tr, vlacdif, vgluout, vcI, vsdh, vcIII, vcs, vasp_atf, vasp_atr, vGl, vldh, vaspout, vperos, rct, ntmax, katase, glu_o, laco, k_o, Na_o, nNV;
-extern const int fsdh, fsm, fc1c, fme, fpyrt, fcs, ftca, fuoa, fatps, flk, fbp, fc1, fc2, flkc1, nflx;
-extern double tfac, xi1, xi2, x01, x02, x03, x04;
-
